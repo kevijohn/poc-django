d7591bfbeabbc2fcea6bea5a6c6b910c6cfc6ac9
Kevin Johnson <kevijohn@gmail.com>
2023-04-06 22:12:53 -0400

    Create room and room components

--- poc_django/frontend/src/components/App.js
+++ poc_django/frontend/src/components/App.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import HomePage from "./HomePage";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
   Redirect,
+  useParams
 } from "react-router-dom";
 
 export default class App extends Component {
@@ -17,14 +19,15 @@ export default class App extends Component {
   }
 
   render() {
-    
+    const { roomCode } = useParams();
     return <div>
-        <h1>testing react code blah</h1>
+        <h1>This is in app.js</h1>
         <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage name='Kevs' />}/>
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
+        <Route path="/room/:roomCode" element={<Room />} />
       </Routes>
     </BrowserRouter>
       </div>;
